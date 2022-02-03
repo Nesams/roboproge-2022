e8cf1586373bbb679902ffb9ce36b19cbb9ae2b6
Nele Liis Sams <nesams@ttu.ee>
2022-02-03 14:01:11 +0200

    here we go

--- OT03/robot.py
+++ OT03/robot.py
@@ -44,6 +44,8 @@ class Robot:
 
     def sense(self):
         """Read the sensor values from the PiBot API."""
+        print(self.robot.get_rigt_wheel_encoder())
+        print(self.robot.get_left_wheel_encoder())
         self.right_history.append(self.robot.get_right_wheel_encoder())
         self.left_history.append(self.robot.get_left_wheel_encoder())
         self.reading_times.append(self.robot.get_time())
