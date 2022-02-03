3203e0eb65f98d57740bc1f201281d57e15fd9ae
Nele Liis Sams <nesams@ttu.ee>
2022-02-03 15:08:03 +0200

    here we go

--- OT04/OT04.py
+++ OT04/OT04.py
@@ -15,7 +15,7 @@ class Robot:
         self.third_from_right = 0
         self.second_from_right = 0
         self.rightmost = 0
-        self.line = [self.leftmost, self.second_from_left, self.third_from_right, self.third_from_left, self.second_from_right, self.rightmost]
+        self.line_directions = [self.leftmost, self.second_from_left, self.third_from_right, self.third_from_left, self.second_from_right, self.rightmost]
 
     def set_robot(self, robot: PiBot.PiBot()) -> None:
         """Set robot reference."""
@@ -29,6 +29,17 @@ class Robot:
         self.third_from_right = self.robot.get_third_line_sensor_from_right()
         self.second_from_right = self.robot.get_second_line_sensor_from_right()
         self.rightmost = self.robot.get_rightmost_line_sensor()
+
+    def get_line_direction(self):
+        """
+        Return the direction of the line based on sensor readings.
+
+        Returns:
+          -1: Line is on the right (i.e., the robot should turn right to reach the line again)
+           0: Robot is on the line (i.e., the robot should not turn to stay on the line) or no sensor info
+           1: Line is on the left (i.e., the robot should turn left to reach the line again)
+        """
+        return self.line_directions
 
     def spin(self):
         """The main spin loop."""
