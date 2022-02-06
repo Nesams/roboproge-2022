713881489c3db9f49bffaf4eae351ca9f36d4dcc
Nele Liis Sams <nesams@ttu.ee>
2022-02-06 17:35:23 +0200

    here we go

--- L1/robot.py
+++ L1/robot.py
@@ -79,7 +79,7 @@ class Robot:
     def move_forward(self):
         """Act method for moving forward."""
         self.robot.set_wheels_speed(self.speed)
-        self.robot.sleep(0.005)
+        self.robot.sleep(0.02)
         self.robot.set_wheels_speed(0)
 
     def move_right(self):
