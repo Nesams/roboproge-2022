be1e6fa20e527e371d85ede629141a64b1f0f5fb
Nele Liis Sams <nesams@ttu.ee>
2022-02-17 17:47:36 +0200

    eksnäis

--- OT05/OT05.py
+++ OT05/OT05.py
@@ -1,17 +1,16 @@
 """OT05 - Noise."""
+import PiBot
 import copy
 import statistics
 
-import PiBot
 
 class Robot:
-    """Robot class."""
+    """Robot  class."""
 
     def __init__(self):
         """Initialize object."""
         self.robot = PiBot.PiBot()
         self.shutdown = False
-        self.line_directions = []
         self.front_middle_laser = None
         self.filter_list = []
 
@@ -19,7 +18,7 @@ class Robot:
         """Set the PiBot reference."""
         self.robot = robot
 
-    def get_front_middle_laser(self):
+    def get_front_middle_laser(self) -> float:
         """
         Return the filtered value.
 
@@ -32,15 +31,29 @@ class Robot:
             return statistics.median(self.filter_list)
 
     def sense(self):
-        self.front_middle_laser = self.get_front_middle_laser()
+        """
+        Save sensor data into mutators.
+        """
+        self.front_middle_laser = self.robot.get_front_middle_laser()
         self.filter_list.insert(0, copy.deepcopy(self.front_middle_laser))
         self.filter_list = self.filter_list[:5]
+
+    def plan(self):
+        """Devise a plan of action according to the data."""
+        pass
+
+    def act(self):
+        """Act according to the plan."""
+        pass
 
     def spin(self):
         """The spin loop."""
         while not self.shutdown:
             print(f'Value is {self.get_front_middle_laser()}')
             self.robot.sleep(0.05)
+            self.sense()
+            self.plan()
+            self.act()
             if self.robot.get_time() > 20:
                 self.shutdown = True
 
@@ -49,6 +62,19 @@ def main():
     """The main entry point."""
     robot = Robot()
     robot.spin()
+
+
+def a_test():
+    """
+    Help.
+    """
+    robot = Robot()
+    import dataset1  # or any other data file
+    data = dataset1.get_data()
+    robot.robot.load_data_profile(data)
+    for i in range(len(data)):
+        print(f"laser = {robot.robot.get_front_middle_laser()}")
+        robot.robot.sleep(0.05)
 
 
 if __name__ == "__main__":
