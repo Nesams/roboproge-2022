ddca940ffb8faa3b60d7d3320e62b5d870a530e9
Nele Liis Sams <nesams@ttu.ee>
2022-02-17 17:37:30 +0200

    eksnäis

--- OT06/OT06.py
+++ OT06/OT06.py
@@ -51,7 +51,7 @@ class Robot:
                 object_angle = (self.object_start_and_end[0] + self.object_start_and_end[-1]) / 2
                 self.object_detected = False
                 self.objects.append(object_angle)
-                if 300 <= self.object_start_and_end[0] <= 360 and 0 <= self.object_start_and_end[-1]:
+                if 310 <= self.object_start_and_end[-1] <= 360 and 0 <= self.object_start_and_end[0]:
                     self.objects[-1] += 180
                 self.object_start_and_end.clear()
         return self.objects
