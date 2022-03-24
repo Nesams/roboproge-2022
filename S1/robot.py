e0d52b058541cd2abf2dacede0cd6b34fa10effb
Nele Liis Sams <nesams@ttu.ee>
2022-03-24 17:47:56 +0200

    S1

--- S1/robot.py
+++ S1/robot.py
@@ -246,7 +246,7 @@ class Robot:
             self.left_wheel_speed = math.radians(self.left_delta) / self.delta_time
             self.right_wheel_speed = math.radians(self.right_delta) / self.delta_time
             self.encoder_odometry[2] += (self.wheel_radius / self.robot.AXIS_LENGTH) * (
-                        self.right_encoder - self.left_encoder)
+                        math.radians(self.right_encoder) - math.radians(self.left_encoder))
             self.encoder_odometry[0] += (self.wheel_radius / 2) * (self.left_wheel_speed + self.right_wheel_speed) * math.cos(
                 self.encoder_odometry[2]) * self.delta_time
             self.encoder_odometry[1] += (self.wheel_radius / 2) * (self.left_wheel_speed + self.right_wheel_speed) * math.sin(
