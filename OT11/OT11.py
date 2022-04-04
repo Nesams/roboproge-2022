7990fc0d7d6c2a55272c20b5bad10fa9c0f14a28
Nele Liis Sams <nesams@ttu.ee>
2022-04-04 14:32:56 +0300

    01

--- OT11/OT11.py
+++ OT11/OT11.py
@@ -121,7 +121,7 @@ class Robot:
                 self.closest_object = object
         if self.closest_object > 0:
             angle = math.atan2(self.world_objects[self.closest_object][1] - self.encoder_odometry[1], self.world_objects[self.closest_object][0] - self.encoder_odometry[0]) - self.encoder_odometry[2]
-            return angle
+            return angle + math.radians(270)
         else:
             return None
 
