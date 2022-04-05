ef0865e6c63ac3ee6a5ee655fbedcb05e3622ceb
Nele Liis Sams <nesams@ttu.ee>
2022-04-05 12:19:57 +0300

    01

--- S1/robot.py
+++ S1/robot.py
@@ -109,6 +109,9 @@ class Robot:
         self.red_object_angle = None
         self.blue_object_angle = None
 
+        self.red_object_angle_deg = None
+        self.blue_object_angle_deg = None
+
         self.red = None
         self.blue = None
 
